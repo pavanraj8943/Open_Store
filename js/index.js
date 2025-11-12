d68dc97452869fbdbee50182eb2b3991815c0aa1
pavanraj8943 <pavan.64j@gmail.com>
2025-11-12 14:13:19 +0530

    pending

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const data = ""
+async function fetchData() {
+    try {
+        const res = await fetch('https://dummyjson.com/products')
+        const data = await res.json()
+        let str = ""
+        data.products.forEach(ele => {
+            str += `
+             <div class="card">
+            <img src="${ele.thumbnail}">
+            <a href="./product.html/?id=${ele.id}">
+            <h1>${ele.title}</h1>
+            <p>${ele.price}</p>
+            <p>${ele.rating}
+               </div>
+            <a>
+            `
+        });
+        document.getElementById("cards").innerHTML = str
+    }
+    catch (error) {
+        console.log(error);
+
+    }
+}
+fetchData()
